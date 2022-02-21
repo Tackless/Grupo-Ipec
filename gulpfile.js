e748efe479ca12fb5cad4283558b225d6e5353b5
Donovan Eliosa <oswaldo.d.b.e@hotmail.com>
2022-02-21 00:41:30 -0600

    no notify

--- gulpfile.js
+++ gulpfile.js
@@ -44,14 +44,16 @@ function imagenes() {
     return src(paths.imagenes)
         .pipe(cache(imagemin({ optimizationLevel: 3})))
         .pipe(dest('public/build/img'))
-        .pipe(notify({ message: 'Imagen Completada'}));
+        // .pipe(notify({ message: 'Imagen Completada'}))
+        ;
 }
 
 function versionWebp() {
     return src(paths.imagenes)
         .pipe( webp() )
         .pipe(dest('public/build/img'))
-        .pipe(notify({ message: 'Imagen Completada'}));
+        // .pipe(notify({ message: 'Imagen Completada'}))
+        ;
 }
 
 
